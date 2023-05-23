d3fb186ab26a92ad9d2c447374f96aaa1e9967fb
Douglas Berkley <douglasmberkley@gmail.com>
2023-05-23 09:51:44 +0900

    Added search ajax

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,5 +5,12 @@ class MoviesController < ApplicationController
     if params[:query].present?
       @movies = @movies.where("title ILIKE ?", "%#{params[:query]}%")
     end
+    # we can controller how our controller actions respond
+    # by default, it's just responding to HTML. but we want HTML AND text
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.json { render json: @movies }
+      format.text { render partial: "movies/list", locals: { movies: @movies }, formats: [:html] }
+    end
   end
 end
